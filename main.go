a36bced1b3ad30971037b21876e39d2a239e8b6b
Zach <zachisaunders12@gmail.com>
2024-09-17 16:19:10 -0400

    Added some more

--- main.go
+++ main.go
@@ -5,5 +5,14 @@ import "github.com/xuri/excelize/v2"
 
 func main() {
 	var rosterPath, templatePath, destinationPath string
-	fmt.Println("What is the path to the roster?")
+	err := error.Error()
+	for err != nil {
+		fmt.Println("What is the path to the roster?")
+	}
+	fmt.Println("What is the path to the base report?")
+	fmt.Scan(&templatePath)
+	fmt.Println("What is the path of the output?")
+	fmt.Scan(&destinationPath)
+
+	
 }
