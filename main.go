1ad0cb0311656581a1b5419b5f05aceb031836b3
Zach <zachisaunders12@gmail.com>
2024-09-17 16:02:34 -0400

    Added some starting info

---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+import "github.com/xuri/excelize/v2"
+
+func main() {
+	var rosterPath, templatePath, destinationPath string
+	fmt.Println("What is the path to the roster?")
+}
