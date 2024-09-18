bb3909bc13a2639e0c76ddec94a1d69833a239bf
Zach Saunders <zachisaunders12@gmail.com>
2024-09-18 19:43:04 -0400

    Added error handling

--- main.go
+++ main.go
@@ -1,19 +1,25 @@
 package main
 
 import "fmt"
-import "github.com/xuri/excelize/v2"
 
 func main() {
 	var rosterPath, templatePath, destinationPath string
 	fmt.Println("What is the path to the roster?")
 	_, err := fmt.Scan(&rosterPath)
 	for err != nil {
-
+		fmt.Println("There was an error. You can try a different path, though!")
+		_, err = fmt.Scan(&rosterPath)
 	}
-	fmt.Println("What is the path to the base report?")
+	_, err = fmt.Println("What is the path to the base report?")
 	fmt.Scan(&templatePath)
+	for err != nil {
+		fmt.Println("There was an error. You can try a different path, though!")
+		_, err = fmt.Scan(&templatePath)
+	}
 	fmt.Println("What is the path of the output?")
-	fmt.Scan(&destinationPath)
-
-	
+	_, err = fmt.Scan(&destinationPath)
+	for err != nil {
+		fmt.Println("There was an error. You can try a different path, though!")
+		_, err = fmt.Scan(&rosterPath)
+	}
 }
