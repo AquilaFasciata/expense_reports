4d85f1cc68dcfd447195081e7989dce5058fd3d4
Zach <zachisaunders12@gmail.com>
2024-09-24 14:26:10 -0400

    Opened paths with excelize

--- main.go
+++ main.go
@@ -1,25 +1,46 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+
+	"github.com/xuri/excelize/v2"
+)
 
 func main() {
 	var rosterPath, templatePath, destinationPath string
 	fmt.Println("What is the path to the roster?")
 	_, err := fmt.Scan(&rosterPath)
 	for err != nil {
-		fmt.Println("There was an error. You can try a different path, though!")
-		_, err = fmt.Scan(&rosterPath)
+		fmt.Println(err)
+		return
 	}
-	_, err = fmt.Println("What is the path to the base report?")
-	fmt.Scan(&templatePath)
+
+	roster, err := excelize.OpenFile(strings.TrimSpace(rosterPath))
+	if err != nil {
+		fmt.Println("Error reading roster: ", err)
+		return
+	}
+
+	fmt.Println("What is the path to the base report?")
+	_, err = fmt.Scan(&templatePath)
 	for err != nil {
-		fmt.Println("There was an error. You can try a different path, though!")
-		_, err = fmt.Scan(&templatePath)
+		fmt.Println(err)
+		return
 	}
+
+	template, err := excelize.OpenFile(strings.TrimSpace(rosterPath))
+	if err != nil {
+		fmt.Println("Error reading template: ", err)
+		return
+	}
+
 	fmt.Println("What is the path of the output?")
 	_, err = fmt.Scan(&destinationPath)
 	for err != nil {
-		fmt.Println("There was an error. You can try a different path, though!")
-		_, err = fmt.Scan(&rosterPath)
+		fmt.Println(err)
+		return
 	}
+
+	println("%v", roster)
 }
