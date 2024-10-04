79817b8c9ef150cfaff96d6b667178b86542f731
Zach <zachisaunders12@gmail.com>
2024-10-04 12:47:21 -0400

    Made it work

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"os"
 	"strings"
+	"time"
 
 	"github.com/xuri/excelize/v2"
 )
@@ -31,7 +32,7 @@ func main() {
 		return
 	}
 
-	template, err := excelize.OpenFile(strings.TrimSpace(rosterPath))
+	template, err := excelize.OpenFile(strings.TrimSpace(templatePath))
 	if err != nil {
 		fmt.Println("Error reading template: ", err)
 		return
@@ -57,11 +58,14 @@ func main() {
 	for _, row := range locations {
 		fmt.Println(row)
 	}
+	fmt.Println("There are", len(locations), "rows")
 
-	firstSheet := roster.GetSheetList()[0]
-	roster_cols, err := roster.GetCols(firstSheet)
+	roster_cols, err := roster.GetCols("Sheet1")
 	err_check(err)
+	fmt.Println("There are", len(roster_cols), "columns in the roster")
+	fmt.Println(roster_cols[2])
 
+	today := time.Now()
 	for i, name := range roster_cols[1] {
 		if name == "" {
 			continue
@@ -70,9 +74,13 @@ func main() {
 		template.SetCellStr("Expense Report Template", "D6", roster_cols[0][i])
 
 		for j := 1; j < len(locations); j++ {
-			if locations[j][0][1:2] == roster_cols[2][i+3] {
-				template.SetCellStr("Expense Report Template", "D8", locations[0][j])
-				template.SetCellStr("Expense Report Template", "F13", locations[0][j])
+			location := get_loc_num(locations[j][0])
+			roster_loc := roster_cols[2][i]
+			if strings.Compare(location, roster_loc) == 0 {
+				template.SetCellStr("Expense Report Template", "D8", locations[j][0])
+				template.SetCellStr("Expense Report Template", "F13", locations[j][0])
+				template.SetCellStr("Expense Report Template", "B13", today.Format("09/12/2024"))
+				break
 			}
 		}
 
@@ -92,4 +100,19 @@ func err_check(err error) {
 		fmt.Println(err)
 		return
 	}
+}
+
+func get_loc_num(location string) string {
+	var result string
+	for _, character := range location {
+		if character == '#' {
+			continue
+		}
+		if character == ' ' {
+			break
+		}
+		result += string(character)
+	}
+
+	return result
 }
